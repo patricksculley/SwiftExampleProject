02573b550297aec0562acc85e04ec904d953735a
Patrick Sculley <patrick@pixelflow.com>
2017-06-04 15:53:32 -0700

    Initial Commit

---
+++ ExampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ExampleProject
+//
+//  Created by Patrick Sculley on 6/4/17.
+//  Copyright © 2017 PixelFlow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
